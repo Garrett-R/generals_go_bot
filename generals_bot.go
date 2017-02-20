f4fa67da5fc7dde968640e54c8c0535637e03c87
Garrett-R <garrettreynolds5@gmail.com>
2017-02-20 00:52:48 -0500

    More flailing

--- generals_bot.go
+++ generals_bot.go
@@ -4,17 +4,25 @@ import (
     "github.com/graarh/golang-socketio"
     "github.com/graarh/golang-socketio/transport"
     "log"
+    //"flag"
+  //  "net/url"
 )
 
 func main() {
+    // TODO: what is `flag` anyway?
+    //serverAddr := flag.String("server-url", "ws://botws.generals.io", "address of generals-api server")
+//    controllerUrl, err := url.Parse(*serverAddr)
     log.Println("1")
-    url := gosocketio.GetUrl("http://botws.generals.io", 80, false)
+//    url := gosocketio.GetUrl("http://botws.generals.io", 80, false)
 //    url := gosocketio.GetUrl("localhost", 80, false)
     log.Println("2")
     transport := transport.GetDefaultWebsocketTransport()
     log.Println("About to connect!")
-    client, err := gosocketio.Dial(url, transport)
+    //client, err := gosocketio.Dial(url, transport)
+    client, err := gosocketio.Dial("ws://botws.generals.io", transport)
+    log.Println("Oh hello")
     if err != nil {
+        log.Println("Hmmm")
         log.Fatal(err)
     }
     client.On(gosocketio.OnConnection, func(c *gosocketio.Channel, args interface{}) {
